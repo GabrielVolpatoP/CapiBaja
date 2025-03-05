aa22895089ef42d63d17a11735ff1d81cda5c49a
Gabriel Volpato <74317064+GabrielVolpatoP@users.noreply.github.com>
2025-03-05 12:44:42 -0300

    Add files via upload

--- libraries/Dual_Nucle/Dual_Nucle.cpp
+++ libraries/Dual_Nucle/Dual_Nucle.cpp
@@ -4,7 +4,7 @@
 
 // ------------------------------------------------------------
 
-void Dual_Nucle::criarTarefa(TaskFunction_t receiverTask, const char * receiver, UBaseType_t sizePilha, void * ponteiro, UBaseType_t propriedade, TaskHandle_t handleTask, BaseType_t nucleo){
+void Dual_Nucle::criarTarefa(TaskFunction_t receiverTask, const char * receiver, UBaseType_t sizePilha, void * ponteiro, UBaseType_t propriedade, tskTaskControlBlock** handleTask, BaseType_t nucleo){
 	xTaskCreatePinnedToCore(receiverTask, receiver, sizePilha, ponteiro, propriedade, handleTask, nucleo); 
 }
 
@@ -29,5 +29,5 @@ void Dual_Nucle::enviarFila(QueueHandle_t dadosFila, void * ponteiro){
 // ------------------------------------------------------------
 
 bool Dual_Nucle::receberFila(QueueHandle_t handleFila, void * ponteiro){
-	xQueueReceive(handleFila, &ponteiro, portMAX_DELAY)
+	xQueueReceive(handleFila, &ponteiro, portMAX_DELAY);
 }
